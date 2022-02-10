a90993f171083085dc36d857ce5bd41674863b99
surajgoraya <SurajGoraya@outlook.com>
2022-02-10 16:32:26 -0500

    breaking: svelte init

---
+++ src/lib/Components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+    let current_year = new Date().getFullYear();
+    // export let last_updated;
+</script>
+
+<footer>
+    <hr>
+    <div class="footer">
+        <p class="muted">&copy; suraj goraya {current_year}, made with &hearts; in Canada &nbsp;</p>
+        <!-- <p class="muted"><em>last updated, {last_updated}</em></p> -->
+    </div>
+</footer>
---
+++ src/routes/work-terms/[id].svelte
@@ -0,0 +1,12 @@
+<script context="module">
+	export async function load({params}) {
+
+        //To maintain backwards compact of old URLS in-case they are indexed.
+        
+        if(params.id === '3'){
+            return { status: 410, };
+        } else {
+            return {status: 404};
+        }
+	}
+</script>
