90b186dd44b6d9d6e8c1cf9c39abd23c340b3671
surajgoraya <SurajGoraya@outlook.com>
2022-02-17 00:26:29 -0500

    feat:

--- src/lib/Components/IDKAM.svelte
+++ src/lib/Components/IDKAM.svelte
@@ -1,16 +1,16 @@
 <script>
     import { fade } from 'svelte/transition';
 
-    let vish1 = false;
-    let vish5 = false;
+    let vish1 = true;
+    let vish5 = true;
 
-    setTimeout(()=>{
-        vish1 = true;
-    }, 1000);
+    // setTimeout(()=>{
+    //     vish1 = true;
+    // }, 1000);
 
-    setTimeout(()=>{
-        vish5 = true;
-    }, 2500);
+    // setTimeout(()=>{
+    //     vish5 = true;
+    // }, 2500);
 
     console.log('%c ♥ ', "font-family: 'Helvetica', sans-serif; font-size: 8em; font-weight: 100; color: hsl(355, 52%, 42%); background-color: hsl(0deg 0% 96%); ");
 </script>
@@ -25,10 +25,10 @@
 <div>
     <main>
         {#if vish1}
-        <h1 transition:fade>hey, it was nice to meet you.</h1>
+        <h1 >hey, it was nice to meet you.</h1>
         {/if}
         {#if vish5}
-        <h5 transition:fade>promise me you'll do amazing things?</h5>
+        <h5 >promise me you'll do amazing things?</h5>
         {/if}
     </main>
 </div>
