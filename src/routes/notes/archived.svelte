18d1f437e6a924e3a16fe6523bf96c4ff9964f1b
surajgoraya <SurajGoraya@outlook.com>
2022-09-27 20:24:02 -0400

    fix(notes): :ambulance: temp have a copy in notes to help fix 404 from DNS cache

---
+++ src/routes/notes/archived.svelte
@@ -0,0 +1,19 @@
+<script>
+ import Top from "$lib/Components/Top.svelte";
+</script>
+<title>suraj goraya - archived</title>
+
+<Top/>
+<h1>content archived.</h1>
+<i><p>Hey there! I'm sorry but the page you requested has been archived, and is no longer available :(.</p></i>
+<p>This is likely because the content on the page is old, or maybe from my undergraduate work and is no longer relevant. 
+If by chance there's a reason you'd like it to be put back up, please reach out to this email: archive[@]surajgoraya.ca. I'd be happy to help!</p>
+
+<p class="highlight">-Nice talking to you! See you around on the Internet! Suraj.</p>
+<style>
+    .highlight {
+
+        font-style: italic;
+        color: var(--colour-blossom);
+    }
+</style>
