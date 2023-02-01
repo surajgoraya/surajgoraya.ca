3e40ddb3f5535b17f881664ca002da4cfb98187e
surajgoraya <SurajGoraya@outlook.com>
2023-01-31 23:29:45 -0500

    feat(logo/menu): :lipstick: move drawing logo, clean up

---
+++ src/lib/Components/Suraj-Drawing.svelte
@@ -0,0 +1,92 @@
+<div class="logo-outer">
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 62.97 96.08"
+		id="logo-svg"
+	>
+		<g id="b629f85f-1875-422b-9a94-04afb01705f7" data-name="Layer 2">
+			<g id="a764f387-49a5-4ea2-8587-40bebc8830da" data-name="Layer 2">
+				<path
+					d="M4.46,21.27C1.55,30,.83,49.65,4.46,58.11,8.94,68.51,33.94,92.5,33.94,92.5s2.7-7.51,4.91-7.37c1.37.09,2.46,4.91,2.46,4.91l4.91-7.37,2.46,4.92,4.91-7.37,2.46-2.46L58.5,63S60.84,53.35,61,48.29C61.11,42,58,26,56.05,18.81c-1-3.75-4.29-9.92-7.37-12.28-6.36-4.87-22.3-6-29.48-2.46C13.7,6.79,6.41,15.44,4.46,21.27Z"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<path
+					d="M13.06,69.16S28.53,53.37,35.68,52.79c6.78-.56,13,2.86,13,2.86L58.5,63"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<polyline
+					points="2.01 43.37 33.94 21.27 59.48 34.92"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<line
+					x1="30.29"
+					y1="68.69"
+					x2="44.96"
+					y2="67.18"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<path
+					d="M10,48.6A15,15,0,0,1,15.12,45a39.32,39.32,0,0,1,11.45-3.28"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<path
+					d="M37.66,42.07a11.54,11.54,0,0,1,5.58-1.79A26.6,26.6,0,0,1,53.8,41.59"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<line
+					x1="2.01"
+					y1="40.92"
+					x2="29.31"
+					y2="19.14"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<line
+					x1="3.12"
+					y1="37.92"
+					x2="26.1"
+					y2="14.85"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<line
+					x1="2.2"
+					y1="35.99"
+					x2="21.82"
+					y2="11.64"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+				<line
+					x1="22.04"
+					y1="29.5"
+					x2="49.51"
+					y2="29.59"
+					style="fill:none;stroke:#fff;stroke-miterlimit:10;stroke-width:3.5px;"
+				/>
+			</g>
+		</g>
+	</svg>
+</div>
+
+<style>
+	
+	/* .logo-outer {
+		border-radius: 10%;
+		background-color: var(--colour-bg);
+		height: 5em;
+		width: 2.5em;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	} */
+
+	#logo-svg path,
+	#logo-svg polyline,
+	#logo-svg line {
+		stroke: var(--colour-text) !important;
+		
+	}
+	#logo-svg {
+		z-index: 3;
+		height: 5rem;
+		aspect-ratio: 1/1;
+	}
+</style>
