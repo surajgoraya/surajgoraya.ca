69280d11ca72ee73af2e49ef684ff33bb9173874
surajgoraya <SurajGoraya@outlook.com>
2023-04-06 17:47:34 -0400

    chore: :loud_sound: testing once more

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,12 +1,10 @@
 /** @type {import('@sveltejs/kit').PageServerLoad} */
 export async function load({request}){
-    console.log(request.headers)
     const IP_ADDR = request.headers.get('x-forwarded-for');
-    console.log(IP_ADDR)
     /**
      * Accurate as of https://ocul.on.ca/ip-addresses
      */
-    if(IP_ADDR !== undefined && IP_ADDR.contains('131.104.')){
+    if(IP_ADDR && IP_ADDR.includes('131.104.')){
         return {special: true, message: "Looks like you're accessing this site form the University of Guelph ❤️, welcome fellow Gryphon."}
     } else {
         return {special: false}
