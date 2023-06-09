580430d10211b591475ba48c6844fbae2665ad65
surajgoraya <SurajGoraya@outlook.com>
2023-06-08 21:47:27 -0400

    chore: prep for .md based pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-node';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+	preprocess: [
+		mdsvex({
+			extensions: ['.md']
+		})],
 	kit: {
 		adapter: adapter(),
 	},
