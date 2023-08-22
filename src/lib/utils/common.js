d451affd2134d0236395df056a8ace1b1976d9c9
surajgoraya <SurajGoraya@outlook.com>
2023-08-22 15:14:38 -0400

    build(research): :green_heart: try and fix build import issues

--- src/lib/utils/common.js
+++ src/lib/utils/common.js
@@ -1,13 +1,16 @@
 import dayjs from "dayjs"
 import customParseFormat from 'dayjs/plugin/customParseFormat'
-import LocalizedFormat from 'dayjs/plugin/LocalizedFormat'
 
 dayjs.extend(customParseFormat);
-dayjs.extend(LocalizedFormat);
 
+/**
+ * Formats a date to be displayed in a friendly way.
+ * @param {String} date The date that is required to be formatted in string representation. 
+ * @returns {String} A formatted string in the standard date format used by Canada.
+ */
 const date_format = (date) =>{
     const date_parsed = dayjs(date, 'YYYY-MM-DD HH:mmz');
-    return date_parsed.format('LLLL');
+    return date_parsed.format('dddd DD MMMM YYYY [at] hh:mmA');
 }
 
 export { date_format }
