549ce61cfeb44eed6c60aaa8793292cf14b1c093
surajgoraya <SurajGoraya@outlook.com>
2023-08-26 17:32:55 -0400

    refactor: :recycle: refactor some code (no clue what it does)

--- src/routes/_suki/cover/+server.js
+++ src/routes/_suki/cover/+server.js
@@ -2,6 +2,13 @@ import { error, json, redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
 export function GET({request}) {
+    
+    /**
+     * No clue what this does any more, just gonna comment it out
+     * and throw up an invalid request for now.
+     * 
+     * Some day i'll remember, hopefully.
+    
     const REQUESTING_HEADERS = request.headers;
     if(REQUESTING_HEADERS.has('referer')){
         const REF_URL = new URL(REQUESTING_HEADERS.get('referer')).pathname;
@@ -9,4 +16,7 @@ export function GET({request}) {
     } else {
         return json({error: "Invalid Request", code: 500});
     }
+    **/
+   
+    return json({error: "Invalid Request", code: 500, additional: "see github"})
 }
