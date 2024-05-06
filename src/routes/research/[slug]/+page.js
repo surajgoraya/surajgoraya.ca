e9fea13714a31c16c0e6ce3794e7c4147576de44
surajgoraya <SurajGoraya@outlook.com>
2024-05-06 19:55:42 -0400

    feat: :fire: restart from the ground up.

--- src/routes/research/[slug]/+page.js
+++
@@ -1,6 +0,0 @@
-import { MarkdownPageLoad } from '$lib/utils/MarkdownTools.js';
-
-export async function load({ params }) {
-  const { slug } = params;
-  return await MarkdownPageLoad(import(`../${slug}.md`));
-}
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-// vite.config.js
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()]
-});
