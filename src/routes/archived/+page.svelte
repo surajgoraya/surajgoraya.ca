679e22ebf349bf30d40888bda5b5ba0ae3d0c33b
surajgoraya <SurajGoraya@outlook.com>
2024-06-18 14:33:36 -0400

    fix: :hamster: archive page was cutting off on sm
    Probably should make this a component later

--- src/routes/archived/+page.svelte
+++ src/routes/archived/+page.svelte
@@ -6,23 +6,23 @@
 
 <Seo title={'archived'} description={DEFAULTS.description} noIndex/>
 <div class=" bg-brand-white">
-	<Container class={'py-56 h-[calc(100vh-22rem)] flex flex-row items-center'}>
+	<Container class={'py-56 min-h-[calc(100vh-22rem)] flex flex-row items-center'}>
 		<div class="flex flex-col gap-2">
 			<i class="bi bi-box2-heart-fill text-5xl opacity-30"></i>
 			<h1 class="font-branding font-black text-brand-dark text-[3rem] lg:text-[4rem]">
 				{`Archived,`}
 			</h1>
-			<h2 class="text-brand-dark text-xl font-sans">
+			<h2 class="text-brand-dark text-xl font-sans mt-2">
 				{`Hey there! I'm sorry but the page you requested has been archived, and is no longer available
 			:(.`}
 			</h2>
-			<p class="text-brand-dark text-lg font-sans mt-2">
+			<p class="text-brand-dark text-md font-sans mt-8">
 				This is likely because the content on the page is old, or maybe from my undergraduate work
 				and is no longer relevant. If by chance there's a reason you'd like it to be put back up,
 				please reach out to this email:&nbsp;<i><u> archive [at] surajgoraya.ca</u></i>. I'd be happy
 				to help!
 			</p>
-            <p class="font-sans italic mt-8">-Nice talking to you! See you around on the Internet! Suraj.</p>
+            <p class="font-sans italic mt-2">-Nice talking to you! See you around on the Internet! Suraj.</p>
 		</div>
 	</Container>
 </div>
