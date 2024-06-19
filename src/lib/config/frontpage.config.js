47b3e25420eac0fbe9ad5b31d308c08f4bd38789
surajgoraya <SurajGoraya@outlook.com>
2024-06-18 23:02:04 -0400

    fix: :flushed: whoops, forgot lorem ipsum

--- src/lib/config/frontpage.config.js
+++ src/lib/config/frontpage.config.js
@@ -3,17 +3,19 @@ const CASE_STUDIES = [
 		title: 'ASSETS 2024 Website and Design System',
 		skills: ['UI/UX', 'Design System', 'Development'],
 		description:
-			'Worked on Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt sed lorem mattis malesuada. In ut enim tempus, condimentum purus vitae, accumsan dui.',
+			'ASSETS is an international conference on computers and accessibility, and is regarded as the premier conference on the subject. I worked on redesigning the consumer facing website for 2024, developing a Design System, and website design from the ground up.',
 		image: '/assets/works/ASSETS2024.jpg',
+		alt: "A screenshot of the ASSETS 2024 website. On a background of St. John's Newfoundland, there is a semi-translucent black box which says in all capital letters ASSETS 2024.",
 		url: '/portfolio/assets-2024/website',
 		locked: false
 	},
 	{
 		title: 'ASSETS 2024 Branding',
-		skills: ['Branding'],
+		skills: ['Branding', 'Logo Design'],
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt sed lorem mattis malesuada. In ut enim tempus, condimentum purus vitae, accumsan dui.',
+			'Alongside my work with my teammates developing a design system and website for ASSETS 2024, I was the primary designer of the ASSETS 2024 logo, as well as brand identity.',
 		image: '/assets/works/ASSETS2024-branding.jpg',
+		alt: "A slightly blurred out white lighthouse in the background, on a clifftop with a house. In the foreground is the ASSETS 2024 logo. It is displaying a lighthouse (similar to the one in the background to the left and the words 'assets' in the middle.",
 		url: '/portfolio/assets-2024/branding',
 		locked: true
 	}
@@ -29,7 +31,7 @@ const CASE_STUDIES = [
 
 const RESEARCH = [
 	{
-		title: '',
+		title: 'DESIGN SYSTEMS',
 		type: 'Graduate Thesis',
 		description: '',
 		area: ['design systems'],
@@ -43,6 +45,7 @@ const RESEARCH = [
 		description: 'Analyzing the state of design systems in Canadian e-Government initiatives',
 		area: ['design systems', 'e-government'],
 		image: '/assets/research/ds-and-gov.jpg',
+		alt: 'A silhouette of the Canadian House of Commons and Peace Tower near the river during sunrise,',
 		url: '/research/ds-and-gov',
 		ongoing: false
 	}
