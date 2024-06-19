0642688a8f22cf5dbbe8ed3e7d9509e3074f98fd
surajgoraya <SurajGoraya@outlook.com>
2024-06-18 23:27:23 -0400

    fix: :lipstick: small issue with system pages

--- src/lib/components/templates/SystemPage.svelte
+++ src/lib/components/templates/SystemPage.svelte
@@ -2,6 +2,8 @@
 	import Container from '$lib/components/core/Container.svelte';
 </script>
 
-<Container class={'bg-brand-white py-56 min-h-[calc(100vh-22rem)] flex flex-row items-center'}>
-	<slot />
-</Container>
+<div class="bg-brand-white">
+	<Container class={'py-56 min-h-[calc(100vh-22rem)] flex flex-row items-center'}>
+		<slot />
+	</Container>
+</div>
