7dff4cce7b35586c89ee3a83a2c749b8db0be263
surajgoraya <4347494+surajgoraya@users.noreply.github.com>
2026-08-22 19:02:03 -0400

    Revert "chore: move from vercel"
    This reverts commit 34dc56e231ad3688ac6a28bc0ce9980de83647e7.

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,17 +2,11 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		interface Platform {
-			env: Env;
-			ctx: ExecutionContext;
-			caches: CacheStorage;
-			cf?: IncomingRequestCfProperties
-		}
-
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
+		// interface Platform {}
 	}
 }
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,21 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 /** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = { extensions: ['.md'] };
+const mdsvexOptions = {
+	extensions: ['.md']
+};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	kit: { adapter: adapter() },
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	},
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)]
 };
 
